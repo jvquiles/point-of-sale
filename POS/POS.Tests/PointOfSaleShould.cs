b5360ad189c7d21f6d43770d74bc6626615110a5
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-06 13:23:50 +0200

    chore: Create project

---
+++ POS/POS.Tests/PointOfSaleShould.cs
@@ -0,0 +1,16 @@
+namespace POS.Tests
+{
+    public class PointOfSaleShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
